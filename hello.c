52a3ec4408321972d3994e6c6fd6fc3876ca1e15
PawEhThaYou <pthayou2@huskers.unl.edu>
2021-08-27 21:18:12 +0000

    added code

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: PawEhThaYou
+ * Date: 2021/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("PawEhThaYou\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
